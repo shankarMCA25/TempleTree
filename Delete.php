1e8d115879d09fe7e8e0757a4dffa4422169e843
shankarMCA25 <shankar.bevinkatti@gmail.com>
2016-03-17 00:26:25 +0530

    Create Delete.php

---
+++ Delete.php
@@ -0,0 +1,10 @@
+<?php
+  include 'header.php';
+  $databse=new dbconn();
+  $var = $_GET['Emp_id'];
+  $res=$database->update_emp_status("Employee","Emp_Status","'Emp_id'='$var'");
+  if($res)  
+    return true;
+  else
+    return false;
+?>
